d82283383ba0260fcbca65a64b774ce387fa5890
Josep434t <Junioryeri4zion@gmail.com>
2025-09-17 09:14:13 -0600

    Changed things in the mobile view and desktop veiw for the Header, and mobile veiw for the content and Apartment

--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -13,144 +13,152 @@ import size from "../assets/icons/size.png";
 
 export default function Apartments() {
   return (
-    <div class="p-20">
-      <div class="pb-30">
+    <div class="pt-25 md:p-20">
+      <div class="pb-12 md:pb-30">
         <img src={line} alt="" class="pb-7" />
-        <h1 class="font-semibold text-6xl">
-          <p>Find your next place live</p>
+        <h1 class="text-4xl font-semibold md:text-6xl">
+          <p>Find your next place to live</p>
         </h1>
       </div>
-      <div class="flex gap-50 px-5">
+      <div class="text-sm flex gap-5 md:gap-50 md:px-5">
         <div>
-          <a href="" class="flex items-center gap-2 font-semibold"><p>Looking for</p> <img src={adown} alt="" className="h-fit" /></a>
+          <a href="" class="flex items-center gap-2 font-semibold">
+            <p>Looking for</p> <img src={adown} alt="" className="h-fit" />
+          </a>
         </div>
         <div>
-          <a href="" class="flex items-center gap-2 font-semibold"><p>Location</p> <img src={adown} alt="" className="h-fit" /></a>
+          <a href="" class="flex items-center gap-2 font-semibold">
+            <p>Location</p> <img src={adown} alt="" className="h-fit" />
+          </a>
         </div>
         <div>
-          <a href=""  class="flex items-center gap-2 font-semibold"><p>Property Type</p> <img src={adown} alt="" className="h-fit" /></a>
+          <a href="" class="flex items-center gap-2 font-semibold">
+            <p>Property Type</p> <img src={adown} alt="" className="h-fit" />
+          </a>
         </div>
         <div>
-          <a href="" class="flex items-center gap-2 font-semibold"><p>Price</p> <img src={adown} alt="" className="h-fit" /></a>
+          <a href="" class="flex items-center gap-2 font-semibold">
+            <p>Price</p> <img src={adown} alt="" className="h-fit" />
+          </a>
         </div>
       </div>
-      <div class="flex flex-row pt-30 justify-center">
-        <div Class="grid grid-cols-3 gap-6">
-          <p class="font-semibold text-[25px]">
+      <div class="flex flex-row pt-25 md:pt-30 justify-center">
+        <div Class="grid grid-cols-3 gap-5 md:gap-6">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp1} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
             </a>
           </p>
-          <p class="font-semibold text-[25px]">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp2} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
             </a>
           </p>
-          <p class="font-semibold text-[25px]">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp3} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
             </a>
           </p>
-          <p class="font-semibold text-[25px]">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp4} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
             </a>
           </p>
-          <p class="font-semibold text-[25px]">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp5} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
             </a>
           </p>
-          <p class="font-semibold text-[25px]">
+          <p class="font-semibold text-[20px] md:text-[25px]">
             <img src={Exp6} alt="" class="pb-5" />
             <a href="">
               Malto House
-              <div class="flex gap-12 items-center justify-center">
-                <div class="flex items-center gap-3">
-                  <img src={bed} alt="" class="h-6" />
+              <div class="flex gap-2 text-sm md:gap-12 items-center justify-center">
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={bed} alt="" class="h-5 md:h-6" />
                   <p>4</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={Shower} alt="" class="h-15" />
+                <div class="flex items-center md:gap-3">
+                  <img src={Shower} alt="" class="h-10 md:h-15" />
                   <p>2</p>
                 </div>
-                <div class="flex items-center gap-3">
-                  <img src={size} alt="" class="h-6" />
+                <div class="flex items-center gap-1 md:gap-3">
+                  <img src={size} alt="" class="h-5 md:h-6" />
                   <p>2</p>
                 </div>
               </div>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,7 +6,7 @@ import Line from "../assets/icons/Line.png"
 export default function () {
   return (
     <div>
-      <div className="flex mt-40">
+      <div className="grid-cols-1 md:flex mt-40">
         <div>
           <img src={img1} alt="" class="w-180" />
         </div>
@@ -20,9 +20,10 @@ export default function () {
             involuta aperiri, altera occulta quaedam et voluptatem accusantium
             doloremque.
           </p>
-        <p class="mt-8 bg-black max-w-40 text-white p-5 rounded-tr-xl "><a href="" class="flex items-center gap-3">Learn more<img src={Path1} alt="" class="h-fit" /></a></p>
+        <p class="mt-8 bg-black max-w-40 text-white p-5 rounded-tr-xl "><a href="" class="flex items-center gap-3">Learn more<img src={Path1} alt="" class="md:h-fit w" /></a></p>
         </div>
       </div>
     </div>
   );
 }
+ 
